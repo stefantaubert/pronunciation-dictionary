c8d67bc79a5a5bbe2073b0a864b14055cea987eb
Stefan Taubert <stefan.taubert@posteo.de>
2022-04-14 16:45:03 +0200

    improved pyproject.toml

--- src/pronunciation_dictionary_tests/test_api.py
+++ src/pronunciation_dictionary_tests/test_api.py
@@ -1,1 +1,8 @@
+import pytest
+
 from pronunciation_dictionary.api import validate_dictionary
+
+
+def test_dict__raises_ValueError():
+  with pytest.raises(ValueError):
+    validate_dictionary({})
