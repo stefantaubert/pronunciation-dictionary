80041bc2ae3910988c9fbf0acb048d5e9316fd5d
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2022-12-01 15:52:02 +0100

    added shortest and longest method

--- src/pronunciation_dictionary/__init__.py
+++ src/pronunciation_dictionary/__init__.py
@@ -4,9 +4,11 @@ from pronunciation_dictionary.mp_options import MultiprocessingOptions
 from pronunciation_dictionary.phoneme_set_extraction import get_phoneme_set
 from pronunciation_dictionary.pronunciation_selection import (get_first_pronunciation,
                                                               get_last_pronunciation,
+                                                              get_longest_pronunciation,
                                                               get_pronunciation_with_highest_weight,
                                                               get_pronunciation_with_lowest_weight,
                                                               get_random_pronunciation,
+                                                              get_shortest_pronunciation,
                                                               get_weighted_pronunciation)
 from pronunciation_dictionary.serialization import SerializationOptions, serialize
 from pronunciation_dictionary.types import (Pronunciation, PronunciationDict, Pronunciations,
